a252e7d076213e4b984626538f1fb87c1d91197b
Jared Long <Jaredlong01@gmail.com>
2020-09-01 13:34:00 -0700

    Part1 Commit

--- Day2/part1.py
+++ Day2/part1.py
@@ -1,3 +1,18 @@
 #Reading and pulling the deets from the textFile.
 my_file = open('day2/input.txt', 'r')
 dimensions = my_file.read()
+dimension_list = dimensions.split('\n')
+
+def answer(measurements):
+    total = 0
+    for item in measurements:
+        measurment_list = item.split('x')
+        l = int(measurment_list[0])
+        w = int(measurment_list[1])
+        h = int(measurment_list[2])
+        area = 2*l*w + 2*w*h + 2*h*l
+        extra = min(l*w, w*h, h*l)
+        total = total + area + extra
+    print(total)
+
+answer(dimension_list)
