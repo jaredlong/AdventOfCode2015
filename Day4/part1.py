0be790166c473d69149f6bfa36970eedaaff45fa
Jared Long <jaredlong01@gmail.com>
2020-09-04 19:02:56 -0700

    Day4 Commit

---
+++ Day4/part1.py
@@ -0,0 +1,14 @@
+import hashlib 
+
+secret_code = 0
+
+while True:
+    secret_key = 'iwrupvqb' + str(secret_code)
+    result = hashlib.md5(secret_key.encode()) 
+    answer = result.hexdigest()
+    if answer[0:5] == '00000':
+        break
+    secret_code += 1
+print('The Secret Code', secret_code) 
+print('The Secret Answer', secret_key)
+print('The MD5 Hash Code ', answer)
